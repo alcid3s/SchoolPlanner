bfb8250fc8ed05404d8e2f2c851ea141c6c65a08
Alexander Zwiers <at.zwiers@student.avans.nl>
2022-02-10 11:46:32 +0100

    Room extends

---
+++ src/Canteen.java
@@ -0,0 +1,6 @@
+public class Canteen extends Room{
+
+    public Canteen(String name, int capacity) {
+        super(name, capacity);
+    }
+}
---
+++ src/Xplora.java
@@ -0,0 +1,6 @@
+public class Xplora extends Room{
+
+    public Xplora(String name, int capacity) {
+        super(name, capacity);
+    }
+}
