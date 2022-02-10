991585f16b86403c6f53ddf3b22cb2ce49071766
Alexander Zwiers <at.zwiers@student.avans.nl>
2022-02-10 11:39:13 +0100

    basic

---
+++ src/Group.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Group{
+
+    private ArrayList<Person> students;
+
+    public Group(){
+        this.students = new ArrayList<>();
+    }
+}
---
+++ src/Room.java
@@ -0,0 +1,10 @@
+public abstract class Room{
+
+    private String name;
+    private int capacity;
+
+    public Room(String name, int capacity){
+        this.name = name;
+        this.capacity = capacity;
+    }
+}
