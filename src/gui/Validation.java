0b4c584a658fbaf77557c3e738391dec6700dced
Bram Laros <bramlaros@gmail.com>
2022-02-11 15:46:09 +0100

    Added methods to validation

--- src/gui/Validation.java
+++ src/gui/Validation.java
@@ -1,11 +1,32 @@
 package gui;
 
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 public class Validation{
 
-    public static boolean timeIsValid(String startTime, String endTime){
-        if(startTime.length() >= 4 && startTime.length() <= 5 && endTime.length() >= 4 && endTime.length() <= 5){
-
+        public static boolean sizeIsValid(int capacity, int size){
+        if (size > capacity ){
+            return false;
+        }else{
+            return true;
         }
-        return false;
     }
+
+    public static boolean timeIsValid(LocalTime startTime, LocalTime endTime){
+        int startMinute = startTime.getMinute();
+        int startHour = startTime.getHour();
+        int endMinute = endTime.getMinute();
+        int endHour = endTime.getHour();
+        if ((startHour*100) + startMinute > (endHour*100) + endMinute){
+            return false;
+        }if (startHour > 23||startHour < 0||endHour > 23||endHour < 0){
+            return false;
+        }if (startMinute > 59||startMinute < 0||endMinute > 59||endMinute < 0){
+            return false;
+        }
+        return true;
+    }
+
+    
 }
