e78fd8fe11165777d5e437b2943eb6def3c367b9
Alexander Zwiers <at.zwiers@student.avans.nl>
2022-03-10 14:14:15 +0100

    added camera

--- src/gui/tabs/SimulationTab.java
+++ src/gui/tabs/SimulationTab.java
@@ -1,29 +1,30 @@
 package gui.tabs;
 
-import data.Schedule;
-import data.tilted.TiledImageLayer;
 import data.tilted.TiledMap;
-import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Tab;
 import javafx.scene.layout.BorderPane;
 import org.jfree.fx.FXGraphics2D;
+import org.jfree.fx.Resizable;
+import org.jfree.fx.ResizableCanvas;
 
+import java.awt.*;
 import java.awt.geom.AffineTransform;
-import java.util.Optional;
-import java.util.Scanner;
 
-public class SimulationTab extends Tab {
+public class SimulationTab extends Tab implements Resizable{
     private BorderPane mainPane;
-    private Canvas canvas;
+    private ResizableCanvas canvas;
     private TiledMap map;
+    private Camera camera;
 
-    public SimulationTab() {
+    public SimulationTab(){
         super("Simulation");
         map = new TiledMap("School_Map.json");
         setClosable(false);
 
         mainPane = new BorderPane();
-        canvas = new Canvas();
+        canvas = new ResizableCanvas(e -> draw(e), mainPane);
+        FXGraphics2D g = new FXGraphics2D(canvas.getGraphicsContext2D());
+        camera = new Camera(canvas, this, g);
 
         if(mainPane.getHeight() == 0 || mainPane.getWidth() == 0){
             canvas.setWidth(1920);
@@ -34,18 +35,31 @@ public class SimulationTab extends Tab {
         }
         mainPane.setTop(canvas);
         setContent(mainPane);
-        draw( new FXGraphics2D(canvas.getGraphicsContext2D()));
+        draw(g);
 
     }
 
+    @Override
+    public void draw(FXGraphics2D g2d){
+        g2d.setTransform(new AffineTransform());
+        g2d.setColor(Color.white);
+        g2d.clearRect(0,0, 1920, 1080);
 
-    public void draw(FXGraphics2D graphics) {
-        AffineTransform transform = graphics.getTransform();
-        graphics.clearRect(0, 0, (int)canvas.getWidth(), (int)canvas.getHeight());
-        transform.scale(0.5,0.5);
-        graphics.setTransform(transform);
-        map.draw(graphics);
+        AffineTransform originalTransform = g2d.getTransform();
 
+        g2d.setTransform(camera.getTransform((int)canvas.getWidth(), (int)canvas.getHeight()));
+        g2d.scale(1,-1);
+
+
+//        for(GameObject go : gameObjects) {
+//            go.draw(g2d);
+//        }
+
+//        if(debugSelected) {
+//            g2d.setColor(Color.blue);
+//            DebugDraw.draw(g2d, world, 100);
+//        }
+        g2d.setTransform(originalTransform);
+        map.draw(g2d);
     }
-
 }
