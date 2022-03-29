6106458d6672e0bbd3c86a30597a011a3adbb3ef
Alcides <guussaarloos@outlook.com>
2022-03-29 16:50:33 +0200

    Added more student sprites

--- src/data/persons/Student.java
+++ src/data/persons/Student.java
@@ -6,13 +6,11 @@ import tasks.IdleTask;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Objects;
+import java.util.*;
 
 public class Student extends Person {
-    private static HashMap<Facing, BufferedImage[]> images;
+    private static Random random = new Random();
+    private static ArrayList<HashMap<Facing, BufferedImage[]>> images;
 
     public Student(String name) {
         super(name, getAnimation());
@@ -20,33 +18,37 @@ public class Student extends Person {
 
     private static Animation getAnimation() {
         Animation animation = new Animation(3);
-        if (images != null) {
-            images.forEach(animation::setFacing);
-            return animation;
-        }
-        images = new HashMap<>();
-        final int size = 32;
-        ArrayList<Facing> faces = new ArrayList<>(Arrays.asList(Facing.SOUTH, Facing.WEST, Facing.EAST, Facing.NORTH));
-        try {
-            BufferedImage totalImage = ImageIO.read(Objects.requireNonNull(Student.class.getClassLoader().getResource("NPCs.png")));
-            for (int y = 0; y < 4; y++) {
-                BufferedImage[] sprites = new BufferedImage[3];
-                for (int x = 0; x < 3; x++) {
-                    sprites[x] = totalImage.getSubimage(x * size, y * size, size, size);
+        if(images == null) {
+            images = new ArrayList<>();
+            final int size = 32;
+            ArrayList<Facing> faces = new ArrayList<>(Arrays.asList(Facing.SOUTH, Facing.WEST, Facing.EAST, Facing.NORTH));
+
+            for (int i = 0; i < 4; i++) {
+                HashMap<Facing, BufferedImage[]> temp = new HashMap<>();
+
+                try {
+                    BufferedImage totalImage = ImageIO.read(Objects.requireNonNull(Student.class.getClassLoader().getResource("NPCs.png")));
+                    for (int y = 0; y < 4; y++) {
+                        BufferedImage[] sprites = new BufferedImage[3];
+                        for (int x = 0; x < 3; x++) {
+                            sprites[x] = totalImage.getSubimage(x * size + (i * (32 * 3)), y * size, size, size);
+                        }
+
+                        animation.setFacing(faces.get(y), sprites);
+                        temp.put(faces.get(y), sprites);
+                        if (faces.get(y).equals(Facing.SOUTH)) {
+                            animation.setFacing(Facing.STATIONARY, sprites);
+                            temp.put(Facing.STATIONARY, sprites);
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-                animation.setFacing(faces.get(y), sprites);
-                images.put(faces.get(y), sprites);
-                if (faces.get(y).equals(Facing.SOUTH)) {
-                    animation.setFacing(Facing.STATIONARY, sprites);
-                    images.put(Facing.STATIONARY, sprites);
-                }
+                images.add(temp);
             }
-            return animation;
-        } catch (IOException e) {
-            e.printStackTrace();
-            images = null;
-            return null;
         }
+        images.get(random.nextInt(images.size())).forEach(animation::setFacing);
+        return animation;
     }
 
     @Override
