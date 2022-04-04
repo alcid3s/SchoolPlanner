53962dc1fa5752a197259b016a0970b26cea8ae1
Tommy <tdc.vanlangevelde@sudent.avans.nl>
2022-04-04 23:15:41 +0200

    Added comments in Task

--- src/tasks/LeaveTask.java
+++ src/tasks/LeaveTask.java
@@ -4,10 +4,28 @@ import data.persons.Person;
 import data.rooms.Exit;
 import data.tiled.TiledMap;
 
+/**
+ * Class LeaveTask
+ * Creates task for Person to leave the school
+ */
+
 public class LeaveTask extends Task {
+
+    /**
+     * Constructor LeaveTask
+     * Creates a LeaveTask for Person
+     * @param p for the Task
+     */
+
     public LeaveTask(Person p) {
         super(p, null, Exit.getInstance());
     }
+
+    /**
+     * Method update
+     * Updates location for Person to go to exit
+     * @param deltaTime to do update
+     */
 
     @Override
     public void update(double deltaTime) {
