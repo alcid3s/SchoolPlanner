8cecc59d85658e5f6afaa739b2a782dcf454b12a
Bram Laros <bramlaros@gmail.com>
2022-04-04 22:53:08 +0200

    Improved third set of comments
    Part2

--- src/data/rooms/Xplora.java
+++ src/data/rooms/Xplora.java
@@ -5,7 +5,25 @@ import data.tiled.TiledMap;
 
 import java.awt.*;
 
+/**
+ * Class Xplora
+ * Extends interface Room and creates Xplora object
+ */
+
 public class Xplora extends Room {
+
+    /**
+     * Constructor Xplora
+     * @param m (=TiledMap) for the object
+     * @param name for the object
+     * @param capacity for the object
+     * @param location for the object
+     * @param target for the object
+     * @param x for the object
+     * @param y for the object
+     * @param width for the object
+     * @param height for the object
+     */
 
     public Xplora(TiledMap m, String name, int capacity, Point location, MapTarget target, int x, int y, int width, int height){
         super(m,name, capacity, target,location, x, y, width, height);
